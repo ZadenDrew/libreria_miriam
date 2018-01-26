f5531958094aea352e25c899e73cdf9e9e38ea02
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-01-26 10:47:24 +0100

    Añadido método librería

--- src/libreria_cod_andrea/Libreria_COD_Andrea.java
+++ src/libreria_cod_andrea/Libreria_COD_Andrea.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package libreria_cod_andrea;
+
+import com.otros.libreria.Libreria;
 
 /**
  *
@@ -17,5 +15,5 @@ public class Libreria_COD_Andrea {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
